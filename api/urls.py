fd3cff8e705c478e6bc24b742629fb111dd7f64d
HAINSIS <abdelhamid.hadili@gmail.com>
2022-01-28 23:05:21 +0100

    creating the getupdate function

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="Notes"),
     path('notes/<str:pk>/update', views.updateNote, name="update-note"),
-
     path('notes/<str:pk>', views.getNote, name="Note"),
 ]
