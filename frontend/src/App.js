e50b32c88d7e37d7495646d3ff37ddab0c057f61
HAINSIS <abdelhamid.hadili@gmail.com>
2022-03-27 21:30:41 +0000

    adding react app to django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header'; 
 import NotesListPage from './pages/NotesListPage';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route
 } from "react-router-dom";
 import NotePage from './pages/NotePage';
